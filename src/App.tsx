88479b5f7633bbd7053762f4919f4afce956535d
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-08-20 16:30:03 +0300

    basic map implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import MapChart from './MapChart'
+
 function App() {
-  return <h1 className="font-bold text-teal-800 text-center">Hello World!</h1>
+  return (
+    <>
+      <h1 className="font-bold text-teal-800 text-center">Hello World!</h1>
+      <MapChart />
+    </>
+  )
 }
 
 export default App
