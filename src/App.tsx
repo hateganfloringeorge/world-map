e9cfb5ef57aa824110d26f690b2bf912a1ce8d80
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-08-25 01:08:13 +0300

    added border to outline map

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import MapChart from './MapChart'
 function App() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <h1 className="font-bold text-teal-800 text-center">Hello World!</h1>
-      <div className="w-2/3">
+      <h1 className="font-bold my-5 text-teal-800 text-center">Hello World!</h1>
+      <div className="w-4/5 border-double border-2 border-black">
         <MapChart />
       </div>
     </div>
