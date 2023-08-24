80ee990f94f3c33414535b958310ae9bc9d44492
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-08-24 22:13:37 +0300

    added basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import MapChart from './MapChart'
 
 function App() {
   return (
-    <>
+    <div className="w-full h-full flex flex-col justify-center items-center">
       <h1 className="font-bold text-teal-800 text-center">Hello World!</h1>
-      <MapChart />
-    </>
+      <div className="w-2/3">
+        <MapChart />
+      </div>
+    </div>
   )
 }
 
