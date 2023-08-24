f0f1bb33202584b308eb765443d0f040424a2288
hateganfloringeorge <hateganfloringeorge@gmail.com>
2023-08-25 01:00:10 +0300

    added zoom functionality

--- src/MapChart.tsx
+++ src/MapChart.tsx
@@ -1,4 +1,9 @@
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  ZoomableGroup
+} from 'react-simple-maps'
 
 const geoUrl =
   'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json'
@@ -6,13 +11,15 @@ const geoUrl =
 export default function MapChart() {
   return (
     <ComposableMap>
-      <Geographies geography={geoUrl}>
-        {({ geographies }) =>
-          geographies.map((geo) => (
-            <Geography key={geo.rsmKey} geography={geo} />
-          ))
-        }
-      </Geographies>
+      <ZoomableGroup>
+        <Geographies geography={geoUrl}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography key={geo.rsmKey} geography={geo} />
+            ))
+          }
+        </Geographies>
+      </ZoomableGroup>
     </ComposableMap>
   )
 }
